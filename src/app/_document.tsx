728d978b2cfcdba11f65fc26639ec44696cc0cc7
SamuelPlaton <platonsam02@gmail.com>
2025-08-22 16:43:02 +0200

    ci: init

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -6,13 +6,7 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <meta charSet="UTF-8" />
-        {/* DM Serif Display, Work Sans &&  Literata fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
